8e83622ec0d4d134b9d899b56b83dbb5651d0644
Kristine Ivanova <83393030+KristineIvanova@users.noreply.github.com>
2024-08-19 22:31:21 +0300

    updated yml

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -22,6 +22,8 @@ export class LoginPage {
             await this.page.waitForTimeout(15000); // Add a delay to ensure the page is fully loaded
             //await this.page.goto('https://e.csdd.lv/');
             await this.page.waitForURL('https://e.csdd.lv/');
+            await this.page.goto('https://e.csdd.lv/');
+
             await this.page.waitForTimeout(15000); // Add a delay to ensure the page is fully loaded
             console.log('Page loaded: https://e.csdd.lv/');
             await this.page.getByText('Autorizēties uzziņai').click();
