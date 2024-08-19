9d0b6183df3cf3d982373399fa18f151196dba16
Kristine Ivanova <83393030+KristineIvanova@users.noreply.github.com>
2024-08-19 23:09:00 +0300

    updated yml

--- tests/find-car-check-empty-results.test.ts
+++ tests/find-car-check-empty-results.test.ts
@@ -2,10 +2,16 @@ import { expect, test } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
 import { LoginData } from '../pages/loginData';
 import { CarSearchPage } from '../pages/carSearchPage';
+import {HomePage} from "../pages/homePage";
 
 test('Verify the car info page shows empty results for invalid search criteria', async ({ page }) => {
+    const homePage = new HomePage(page);
     const loginPage = new LoginPage(page);
     const carSearchPage = new CarSearchPage(page);
+
+    await homePage.navigateToHomePage();
+    const homePageLoaded = await homePage.isHomePageLoaded();
+    expect(homePageLoaded).toBe(true);
 
     await loginPage.navigateToLoginPage();
     await loginPage.login(LoginData);
